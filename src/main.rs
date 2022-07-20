f3565e4d35bcf6969cd3eb7b78b1ea298aad5b92
Evan Mark Hopkins <git@evahop.com>
2022-07-19 21:50:26 -0700

    clears surface

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use winit::{
+    dpi::PhysicalSize,
     event::{Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::Window,
@@ -6,9 +7,31 @@ use winit::{
 
 fn main() {
     let event_loop = EventLoop::new();
-    let _window = Window::new(&event_loop).unwrap();
+    let window = Window::new(&event_loop).unwrap();
 
-    event_loop.run(|event, _, control_flow| {
+    let (device, queue, surface, mut surface_config) = pollster::block_on(async {
+        let instance = wgpu::Instance::new(wgpu::Backends::PRIMARY);
+        let adapter = instance.request_adapter(&Default::default()).await.unwrap();
+        let (device, queue) = adapter
+            .request_device(&Default::default(), None)
+            .await
+            .unwrap();
+
+        let surface = unsafe { instance.create_surface(&window) };
+        let (width, height) = window.inner_size().into();
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: *surface.get_supported_formats(&adapter).first().unwrap(),
+            width,
+            height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        surface.configure(&device, &config);
+
+        (device, queue, surface, config)
+    });
+
+    event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
@@ -22,8 +45,30 @@ fn main() {
                         },
                     ..
                 } => *control_flow = ControlFlow::Exit,
+                WindowEvent::Resized(PhysicalSize { width, height }) => {
+                    surface_config.width = width;
+                    surface_config.height = height;
+                    surface.configure(&device, &surface_config)
+                }
                 _ => (),
             },
+            Event::MainEventsCleared => {
+                let surface_texture = surface.get_current_texture().unwrap();
+                let texture_view = surface_texture.texture.create_view(&Default::default());
+
+                let mut command_encoder = device.create_command_encoder(&Default::default());
+                command_encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                        view: &texture_view,
+                        resolve_target: None,
+                        ops: Default::default(),
+                    })],
+                    ..Default::default()
+                });
+
+                queue.submit(Some(command_encoder.finish()));
+                surface_texture.present()
+            }
             _ => (),
         }
     })
